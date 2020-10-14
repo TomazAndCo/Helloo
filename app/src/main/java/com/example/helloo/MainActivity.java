e9c8b9183f4321e3b21343550eddce692745c427
Tomaz <zAndgit config --global user.email 39697219+TomazAndCo@users.noreply.github.comgit config --global user.email zAnd>
2020-10-14 11:00:15 +0700

    New in

--- app/src/main/java/com/example/helloo/MainActivity.java
+++ app/src/main/java/com/example/helloo/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.helloo;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
